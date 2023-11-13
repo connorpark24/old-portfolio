6ede81feb98897d493f000a2ec54f6f73583193b
Connor Park <connorpark024@gmail.com>
2023-11-13 17:16:17 -0500

    add to template

--- src/styles.js
+++ src/styles.js
@@ -1,10 +1,9 @@
 const styles = {
   paddingX: "sm:px-16 px-6",
   paddingY: "sm:py-16 py-6",
-  padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-primary lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+    "font-semibold text-primary lg:text-[80px] sm:text-[70px] xs:text-[60px] text-[50px] lg:leading-[100px] mt-2",
   heroSubText:
     "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 
