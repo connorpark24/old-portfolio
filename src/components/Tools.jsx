395571db5a65bda5ef21af7e8ee417f687636c01
Connor Park <connorpark024@gmail.com>
2024-01-14 13:34:02 -0500

    update toolkit grid

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -6,16 +6,14 @@ const Tools = () => {
       className="w-full flex flex-col py-12 lg:py-24 mb-8 items-center text-primary font-light"
       id="toolkit"
     >
-      <div className="flex flex-col gap-8">
-        <h1 className="lg:text-7xl md:text-6xl text-5xl text-center">
-          Toolkit
-        </h1>
-        <div className="grid grid-cols-4 lg:grid-cols-6 gap-x-8 lg:gap-y-4 gap-y-2">
+      <div className="flex flex-col gap-8 items-center">
+        <h1 className="lg:text-7xl md:text-6xl text-5xl">Toolkit</h1>
+        <div className="w-4/5 grid grid-cols-4 lg:grid-cols-6 gap-x-4 md:gap-x-8 gap-y-2 md:gap-y-4">
           {technologies.map((technology, index) => (
             <div key={index} className="relative group">
               <img
                 src={technology.icon}
-                className="lg:w-24 lg:h-24 h-20 w-20 rounded-2xl p-2"
+                className="rounded-2xl"
                 alt={technology.name}
               />
               <p className="absolute w-full bottom-0 left-0 text-xs text-center text-white bg-primary bg-opacity-70 rounded-b-3xl opacity-0 group-hover:opacity-100 transition-opacity duration-300 px-2 py-1">
